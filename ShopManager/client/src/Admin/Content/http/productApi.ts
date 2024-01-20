c3b187a54f0f2cf5f6cba23740f3603f1b43c5b4
Колосс Матвій <kolossmatveyka@gmail.com>
2024-01-20 23:19:11 +0200

    add authorize requests

--- ShopManager/client/src/Admin/Content/http/productApi.ts
+++ ShopManager/client/src/Admin/Content/http/productApi.ts
@@ -1,20 +1,25 @@
-import { $host } from '../../../http';
+import { $authhost, $host } from '../../../http';
 import { createLinkWithParameters } from '../../utils/createLinkWithParameters';
 import { IProductCreateFormData } from '../components/Product/ProductCreate/ProductCreateForm';
 import { IProductEditFormData } from '../components/Product/ProductEditForm/ProductEditForm';
 import { IProductsRequest } from '../pages/AdminProducts';
 
 export const createProduct = async (productData:IProductCreateFormData) => {
-    const { data } = await $host.post('api/AdminProducts', productData);
+    const { data } = await $authhost.post('api/AdminProducts', productData);
     return data;
 }
 
 export const editProduct = async (productData:IProductEditFormData) => {
-    const { data } = await $host.put('api/AdminProducts', productData);
+    const { data } = await $authhost.put('api/AdminProducts', productData);
     return data;
 }
 
 export const getProducts = async (request:IProductsRequest) => {
-    const {data} = await $host.get(createLinkWithParameters('api/AdminProducts', request));
+    const {data} = await $authhost.get(createLinkWithParameters('api/AdminProducts', request));
+    return data;
+}
+
+export const deleteProduct = async (id:string) => {
+    const { data } = await $authhost.delete('api/AdminProducts/' + id);
     return data;
 }
