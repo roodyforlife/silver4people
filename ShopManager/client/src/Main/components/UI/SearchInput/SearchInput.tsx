72a3fa102734eb552c99569e04c1fdfe1407f3d3
Matvii Koloss <167910007+matviiKoloss@users.noreply.github.com>
2024-11-09 12:13:41 +0200

    Make Enter click after searching the products

--- ShopManager/client/src/Main/components/UI/SearchInput/SearchInput.tsx
+++ ShopManager/client/src/Main/components/UI/SearchInput/SearchInput.tsx
@@ -13,9 +13,16 @@ interface IProps <T extends FieldValues, TName extends Path<T>> {
 export const SearchInput = <T extends FieldValues, TName extends Path<T>>(props:IProps<T, TName>) => {
   const {t} = useTranslation();
 
+  const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      props.callback()
+  }
+  }
+
   return (
     <div className={cl.input}>
-      <input type="search" placeholder={props.placeholder} disabled={props.disabled} {...props.field} ></input>
+      <input type="search" onKeyPress={(event) => handleEnter(event)} placeholder={props.placeholder} disabled={props.disabled} {...props.field} ></input>
       <button onClick={props.callback}>{t("Find")}</button>
     </div>
   )
